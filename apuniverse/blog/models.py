915b58151de0e863567fa114337c19c6cba519e8
dangerdak <dakshina.scott@gmail.com>
2014-06-17 17:28:33 +0100

    Added str method to post model

--- apuniverse/blog/models.py
+++ apuniverse/blog/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     text = models.TextField()
+
+    def __str__(self):
+        return self.title
