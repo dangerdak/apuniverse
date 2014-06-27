24d424482159edb7403d1bdd7cd442655a9d763a
dangerdak <dakshina.scott@gmail.com>
2014-06-27 19:31:45 +0100

    Gallery list view displays thumbnails associated with each gallery

--- apuniverse/galleries/urls.py
+++ apuniverse/galleries/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, url
-from django.views.generic import ListView
 
-from galleries.models import Image
+from galleries.views import IndexView
 
 urlpatterns = patterns('',
         url(r'^$',
-            ListView.as_view(model=Image,),
-            name='images',
-            ),
+            IndexView.as_view(),
+            name='galleries'),
         )
--- apuniverse/galleries/views.py
+++ apuniverse/galleries/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from galleries.models import Image, Gallery
 
-# Create your views here.
+
+class IndexView(ListView):
+    model = Gallery
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['gallery_list'] = []
+        for instance in Gallery.objects.all():
+            context['gallery_list'].append({
+                'title': instance.title,
+                'image_list': Image.objects.filter(gallery__title=instance.title),
+                })
+        return context
