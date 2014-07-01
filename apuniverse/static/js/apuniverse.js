0deab3983d349581d4c7243da8b51a030afed78d
dangerdak <dakshina.scott@gmail.com>
2014-07-01 09:26:07 +0100

    Large image shows up for each gallery (too large!)

--- apuniverse/static/js/apuniverse.js
+++ apuniverse/static/js/apuniverse.js
@@ -1,1 +1,15 @@
-/* Project specific Javascript goes here. */
+// Gallery
+// Show larger image when thumbnail is clicked
+$(function() {
+	'use strict';
+	$('.gallery').each(function() {
+		var gallery = $(this);
+		var thumbs = gallery.find('.thumbnail');
+		var largeImage = gallery.find('.large-image img');
+		thumbs.bind('click', largeImage, function() {
+			var url = $(this).attr('data-url');
+			largeImage.attr('src', url);
+		})
+	})
+
+});
