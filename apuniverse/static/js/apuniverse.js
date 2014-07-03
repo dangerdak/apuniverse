265bf76c41424027883d6fcb10b26819c97b1e74
dangerdak <dakshina.scott@gmail.com>
2014-07-03 21:09:02 +0100

    Fix endless pagination
    All pages were being loaded with the first AJAX request leading to a
    duplicate of the first page and incorrect scrolling response.
    'paginate' tag must be used in page template, not index template.

--- apuniverse/static/js/apuniverse.js
+++ apuniverse/static/js/apuniverse.js
@@ -1,19 +1,28 @@
-// Gallery
-// Show larger image when thumbnail is clicked
-$(function() {
-	'use strict';
-	$('.gallery').each(function() {
-		var gallery = $(this);
-		var thumbs = gallery.find('.thumbnail');
-		var largeImage = gallery.find('.large-image img');
-		thumbs.bind('click', largeImage, function() {
-			thumbs.css('border', '');
-			var url = $(this).attr('data-url');
-			var alt = $(this).attr('alt').split(' ').slice(0,-1).join(' ');
-			largeImage.attr('src', url);
-			largeImage.attr('alt', alt);
-			$(this).css('border', '1px solid black');
-		})
-	})
+$.endlessPaginate({
+	paginateOnScroll: true,
+	paginateOnScrollMargin: 5,
 
+	// When each page has loaded and been inserted into the DOM
+	onCompleted: function() {
+		// Gallery
+		$('.gallery').each(function() {
+			var gallery = $(this);
+			var thumbs = gallery.find('.thumbnail');
+			var largeImage = gallery.find('.large-image img');
+
+			thumbs.bind('click', largeImage, function() {
+				var url = $(this).attr('data-url');
+				// Remove last word from thumbnail alt value
+				var alt = $(this).attr('alt').split(' ').slice(0,-1).join(' ');
+				largeImage.attr('src', url);
+				largeImage.attr('alt', alt);
+
+				// Clear all thumbnail borders...
+				thumbs.css('border', '');
+				// And apply border only to clicked thumbnail
+				$(this).css('border', '1px solid black');
+			});
+		});
+		// End of gallery-specific js
+	}
 });
