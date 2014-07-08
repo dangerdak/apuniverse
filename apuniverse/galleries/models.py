f3185e5c9b718a1dcc544e3a49dda3e2e8fed0a8
dangerdak <dakshina.scott@gmail.com>
2014-07-08 15:39:14 +0100

    Gallery has draft funtionality
    Instead of auto-hiding empty galleries, user can save as draft

--- apuniverse/galleries/models.py
+++ apuniverse/galleries/models.py
@@ -1,43 +1,76 @@
+from datetime import timedelta
+
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.utils import timezone
 
 from image_cropping import ImageRatioField
 from easy_thumbnails.files import get_thumbnailer
 from taggit.managers import TaggableManager
 
+from galleries.managers import PublishedManager
+
 
 class Gallery(models.Model):
+    # MANAGERS
+    objects = models.Manager()
+    published_objects = PublishedManager()
+
+    # FIELDS
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=40, unique=True)
     project_year = models.IntegerField(max_length=9)
     linked_blog = models.ForeignKey('blog.Post', blank=True, null=True)
     summary = models.TextField(max_length=500, blank=True)
     tags = TaggableManager()
+    STATUS_CHOICES = (('published', 'Published'),
+                      ('draft', 'Draft'),)
+    status = models.CharField(choices=STATUS_CHOICES,
+                              default='published',
+                              max_length=9)
 
+    pub_date = models.DateTimeField('Date published', default=timezone.now,
+                                    editable=False)
     date_created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField('Last Modified', auto_now=True)
 
+    # METHODS
     def blog_url(self):
         if (self.linked_blog):
             return self.linked_blog.get_absolute_url()
         return ''
     blog_url.short_description = 'Blog link'
 
-    class Meta:
-        ordering = ['-project_year', '-date_created']
-        verbose_name_plural = 'galleries'
-
     def save(self, *args, **kwargs):
+        status = self.status
+
+        # If draft, set pub_date very far in the future
+        if status == 'draft':
+            self.pub_date = timezone.now() + timedelta(days=900000)
+
+        if status == 'published' and self.pub_date > timezone.now():
+        # ie if status changed from draft to published
+        # (as draft is only way pub_date can be in future)
+                self.pub_date = timezone.now()
+
+        self.last_modified = timezone.now()
         if not self.id:
-            # Newly created object, so set slug
+        # Newly created object, so set slug and date created
+            self.date_created = timezone.now()
             self.slug = slugify(self.title)
+
         super(Gallery, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ['-project_year', '-date_created']
+        verbose_name_plural = 'galleries'
+
 
 class Image(models.Model):
+    # FIELDS
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=40, unique=True)
     date = models.DateField(blank=True, null=True)
@@ -52,9 +85,7 @@ class Image(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField('Last Modified', auto_now=True)
 
-    class Meta:
-        ordering = ['thumbnail_position', '-date_created']
-
+    # METHODS
     def thumbnail_url(self):
         url = get_thumbnailer(self.image).get_thumbnail({
             'size': (100, 100),
@@ -75,3 +106,5 @@ class Image(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ['thumbnail_position', '-date_created']
--- apuniverse/galleries/views.py
+++ apuniverse/galleries/views.py
@@ -10,42 +10,36 @@ from galleries.models import Image, Gallery
 class GalleryListView(AjaxListView):
     model = Gallery
 
-    # Annotate each gallery with count of associated images
-    # numbered_galleries = Gallery.objects.annotate(Count('image'))
-    # queryset = numbered_galleries.filter(image__count__gt=0)
-
     def get_context_data(self, **kwargs):
         context = super(GalleryListView, self).get_context_data(**kwargs)
         context['gallery_list'] = []
         # So that the context contains the images associated with each gallery
-        for instance in Gallery.objects.all():
+        for instance in Gallery.published_objects.all():
             context['gallery_list'].append({
                 'title': instance.title,
-                'image_list': Image.objects.filter(gallery__title=instance.title),
+                'image_list': Image.objects.filter(
+                    gallery__title=instance.title),
                 'tags': instance.tags,
                 })
         return context
 
 
-class TagGalleryList(AjaxListView):
+class GalleryListByTag(AjaxListView):
     template_name = 'galleries/gallery_list.html'
-
-    # Annotate each gallery with count of associated images
-    # numbered_galleries = Gallery.objects.annotate(Count('image'))
-    # queryset = numbered_galleries.filter(image__count__gt=0)
 
     def get_queryset(self):
         self.tags = get_object_or_404(Tag, name=self.kwargs['tags'].title())
-        return Gallery.objects.filter(tags__name__in=[self.tags])
+        return Gallery.published_objects.filter(tags__name__in=[self.tags])
 
     def get_context_data(self, **kwargs):
-        context = super(TagGalleryList, self).get_context_data(**kwargs)
+        context = super(GalleryListByTag, self).get_context_data(**kwargs)
         context['gallery_list'] = []
         # So that the context contains the images associated with each gallery
         for instance in self.get_queryset():
             context['gallery_list'].append({
                 'title': instance.title,
-                'image_list': Image.objects.filter(gallery__title=instance.title),
+                'image_list': Image.objects.filter(
+                    gallery__title=instance.title),
                 'tags': instance.tags,
                 })
         return context
