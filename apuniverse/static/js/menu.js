298c5f57bb45af0d5aed13156277cf32c6887372
dangerdak <dakshina.scott@gmail.com>
2014-07-09 20:38:52 +0100

    Responsive navbar. Tried column layout

---
+++ apuniverse/static/js/menu.js
@@ -0,0 +1,16 @@
+$(function() {
+	var menuIcon = $('#menu-icon');
+	var menu = $('#main-nav ul');
+	var menuHeight = menu.height();
+
+	$(menuIcon).bind('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	$(window).resize(function() {
+		var w = $(window).width();
+		if(w > 600 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+});
