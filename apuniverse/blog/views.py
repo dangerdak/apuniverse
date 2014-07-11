9dd42a995916954b120ea4992b1abbe32b00d4d0
dangerdak <dakshina.scott@gmail.com>
2014-07-11 19:13:34 +0100

    Month archive mixin works

--- apuniverse/blog/views.py
+++ apuniverse/blog/views.py
@@ -1,28 +1,47 @@
 from django.views.generic import ListView
 from django.views.generic.dates import YearArchiveView, MonthArchiveView
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_object_or_404
 from taggit.models import Tag
 
 from blog.models import Post
 
 
 class MonthArchiveMixin(object):
+    """
+    Mixin adds by-month archive info to context.
+    Adds 'by_year', a list of tuples in the format (year, by_month)
+    where 'by_month is a list of tuples in the format
+    (month, [post_list]). I.e. by_year = [(year, (month, [post_list]))...]
+    Dates in descending order.
+    """
 
     def get_context_data(self, **kwargs):
-        qs = self.get_queryset()
-        try:
-            posts = qs.filter(pub_date__year=self.kwargs['year'])
-        except KeyError:
-            posts = qs.filter(pub_date__year=2014)
         context = super(MonthArchiveMixin, self).get_context_data(**kwargs)
-        all_dates = posts.datetimes('pub_date', 'month', 'DESC')
-        by_month = []
-        for date in all_dates:
-            # Posts for each month
-            # ordered by pub_date (see model meta class)
-            month = date.month
-            by_month.append((date, posts.filter(pub_date__month=month)))
-        context['by_month'] = by_month
+
+        if self.queryset:
+            qs = self.queryset
+        elif self.model.published_objects:
+            qs = self.model.published_objects.all()
+        else:
+            qs = self.model.objects.all()
+
+        all_year_dates = qs.datetimes('pub_date', 'year', 'DESC')
+        by_year = []
+        for year_date in all_year_dates:
+            year = year_date.year
+            # Get all posts for the year
+            posts = qs.filter(pub_date__year=year)
+
+            # Get all months in the year which contain a post
+            all_month_dates = posts.datetimes('pub_date', 'month', 'DESC')
+            by_month = []
+            for date in all_month_dates:
+                # Posts for each month
+                # ordered by pub_date (see model meta class)
+                month = date.month
+                by_month.append((date, posts.filter(pub_date__month=month)))
+            by_year.append((year, by_month))
+        context['by_year'] = by_year
         return context
 
 
@@ -40,7 +59,6 @@ class PostListView(MonthArchiveMixin, ListView):
                 'url': instance.get_absolute_url(),
                 'tags': instance.tags,
             })
-
         return context
 
 
