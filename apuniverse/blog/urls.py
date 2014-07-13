24a05a4153c63821f577dbaab84f5f939d565867
dangerdak <dakshina.scott@gmail.com>
2014-07-13 13:03:57 +0100

    Blank lines for readability

--- apuniverse/blog/urls.py
+++ apuniverse/blog/urls.py
@@ -13,21 +13,25 @@ urlpatterns = patterns('',
         PostListView.as_view(),
         name='blog',
         ),
+
     # Detail view
     url(r'^(?P<pub_year>\d{4})/(?P<pub_month>\w{3})/(?P<slug>[a-zA-Z0-9-]+)/$',
         DetailView.as_view(model=Post,),
         name='blog-detail',
         ),
+
     # Monthly archive view
     url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$',
         PostMonthArchiveView.as_view(),
         name='post_month_archive'
         ),
+
     # Yearly archive view
     url(r'^(?P<year>\d{4})/$',
         PostYearArchiveView.as_view(),
         name='post_year_archive'
         ),
+
     # Category view
     url(r'^tags/(?P<tags>\w*)/$',
         PostListByTag.as_view(),
