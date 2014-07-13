f8a9fe19445ea2c2ab5972ae0aa5c3d28a701697
dangerdak <dakshina.scott@gmail.com>
2014-07-13 13:27:15 +0100

    Add gallery archive links

--- apuniverse/galleries/urls.py
+++ apuniverse/galleries/urls.py
@@ -1,13 +1,21 @@
 from django.conf.urls import patterns, url
 
-from galleries.views import GalleryListView, GalleryListByTag
+from galleries.views import (GalleryListView,
+                             GalleryListByTag,
+                             GalleryYearArchiveView)
 
 urlpatterns = patterns('',
     url(r'^$',
         GalleryListView.as_view(),
         name='galleries'),
 
-    # Category views
+    # Yearly archive view
+    url(r'^(?P<year>\d{4})/$',
+        GalleryYearArchiveView.as_view(),
+        name='gallery_year_archive'
+        ),
+
+    # Category view
     url(r'^tags/(?P<tags>\w*)/$',
         GalleryListByTag.as_view(),
         name='galleriestags'),
--- apuniverse/galleries/views.py
+++ apuniverse/galleries/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.views.generic.dates import YearArchiveView
 # from django.db.models import Count
 
 from taggit.models import Tag
@@ -7,21 +8,76 @@ from endless_pagination.views import AjaxListView
 from galleries.models import Image, Gallery
 
 
+class MonthArchiveMixin(object):
+    """
+    Mixin adds by-month archive info to context.
+    Adds 'by_month', a list of tuples in the format
+    (month, [object_list]).
+    Dates in descending order.
+    """
+
+    def get_context_data(self, **kwargs):
+        context = super(MonthArchiveMixin, self).get_context_data(**kwargs)
+
+        # Determine queryset to be used
+        if self.queryset:
+            qs = self.queryset
+        elif self.model.published_objects:
+            qs = self.model.published_objects.all()
+        else:
+            qs = self.model.objects.all()
+
+        print(self.kwargs['year'])
+
+        # Get all objects for the year
+        objects = qs.filter(pub_date__year=self.kwargs['year'])
+
+        # Get all months in the year which contain an object
+        all_month_dates = objects.datetimes('pub_date', 'month', 'DESC')
+        by_month = []
+        for date in all_month_dates:
+            # Objects for each month
+            # ordered by pub_date (see model meta class)
+            month = date.month
+            by_month.append((date, objects.filter(pub_date__month=month)))
+        context['by_month'] = by_month
+        return context
+
+
 class GalleryListView(AjaxListView):
-    model = Gallery
+    queryset = Gallery.published_objects.all()
 
     def get_context_data(self, **kwargs):
         context = super(GalleryListView, self).get_context_data(**kwargs)
+        # Add images to context
         context['gallery_list'] = []
         # So that the context contains the images associated with each gallery
-        for instance in Gallery.published_objects.all():
+        for instance in self.queryset:
             context['gallery_list'].append({
                 'title': instance.title,
                 'image_list': Image.objects.filter(
                     gallery__title=instance.title),
                 'tags': instance.tags,
                 })
+
+        # Add archive links to context
+        all_year_dates = self.queryset.datetimes('pub_date', 'year', 'DESC')
+        archive_links = []
+        for date in all_year_dates:
+            year = date.year
+            year_count = self.queryset.filter(pub_date__year=year).count()
+            archive_links.append((year, year_count))
+        context['archive_links'] = archive_links
+
         return context
+
+
+class GalleryYearArchiveView(MonthArchiveMixin, YearArchiveView):
+    model = Gallery
+    date_field = 'pub_date'
+    make_object_list = True
+    allow_empty = True
+    allow_future = False
 
 
 class GalleryListByTag(AjaxListView):
