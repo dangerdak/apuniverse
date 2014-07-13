09e8efa610912b3e093a4ea3e98fe52b9a310d9e
dangerdak <dakshina.scott@gmail.com>
2014-07-13 12:01:51 +0100

    Fixed month mixin

--- apuniverse/blog/views.py
+++ apuniverse/blog/views.py
@@ -9,15 +9,15 @@ from blog.models import Post
 class MonthArchiveMixin(object):
     """
     Mixin adds by-month archive info to context.
-    Adds 'by_year', a list of tuples in the format (year, by_month)
-    where 'by_month is a list of tuples in the format
-    (month, [post_list]). I.e. by_year = [(year, (month, [post_list]))...]
+    Adds 'by_month', a list of tuples in the format
+    (month, [post_list]).
     Dates in descending order.
     """
 
     def get_context_data(self, **kwargs):
         context = super(MonthArchiveMixin, self).get_context_data(**kwargs)
 
+        # Determine queryset to be used
         if self.queryset:
             qs = self.queryset
         elif self.model.published_objects:
@@ -25,40 +25,35 @@ class MonthArchiveMixin(object):
         else:
             qs = self.model.objects.all()
 
-        all_year_dates = qs.datetimes('pub_date', 'year', 'DESC')
-        by_year = []
-        for year_date in all_year_dates:
-            year = year_date.year
-            # Get all posts for the year
-            posts = qs.filter(pub_date__year=year)
+        print(self.kwargs['year'])
 
-            # Get all months in the year which contain a post
-            all_month_dates = posts.datetimes('pub_date', 'month', 'DESC')
-            by_month = []
-            for date in all_month_dates:
-                # Posts for each month
-                # ordered by pub_date (see model meta class)
-                month = date.month
-                by_month.append((date, posts.filter(pub_date__month=month)))
-            by_year.append((year, by_month))
-        context['by_year'] = by_year
+        # Get all posts for the year
+        objects = qs.filter(pub_date__year=self.kwargs['year'])
+
+        # Get all months in the year which contain a post
+        all_month_dates = objects.datetimes('pub_date', 'month', 'DESC')
+        by_month = []
+        for date in all_month_dates:
+            # Posts for each month
+            # ordered by pub_date (see model meta class)
+            month = date.month
+            by_month.append((date, objects.filter(pub_date__month=month)))
+        context['by_month'] = by_month
         return context
 
 
-class PostListView(MonthArchiveMixin, ListView):
-    model = Post
+class PostListView(ListView):
+    queryset = Post.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super(PostListView, self).get_context_data(**kwargs)
-        context['post_list'] = []
-        for instance in Post.published_objects.all():
-            context['post_list'].append({
-                'title': instance.title,
-                'text': instance.text,
-                'pub_date': instance.pub_date,
-                'url': instance.get_absolute_url(),
-                'tags': instance.tags,
-            })
+        all_year_dates = self.queryset.datetimes('pub_date', 'year', 'DESC')
+        archive_links = []
+        for date in all_year_dates:
+            year = date.year
+            year_count = self.queryset.filter(pub_date__year=year).count()
+            archive_links.append((year, year_count))
+        context['archive_links'] = archive_links
         return context
 
 
