6bf1f32ac37cbd9dfd297ef925722d146d0eea0e
dangerdak <dakshina.scott@gmail.com>
2014-07-17 19:54:09 +0100

    Colorbox

--- apuniverse/galleries/models.py
+++ apuniverse/galleries/models.py
@@ -77,7 +77,7 @@ class Image(models.Model):
     medium = models.CharField(max_length=200, blank=True)
     size = models.CharField(max_length=50, blank=True)
     image = models.ImageField(upload_to='images/')
-    thumbnail = ImageRatioField('image', '100x100', free_crop=True)
+    thumbnail = ImageRatioField('image', '100x100')
 
     gallery = models.ForeignKey(Gallery)
     thumbnail_position = models.PositiveSmallIntegerField(
