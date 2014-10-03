889c97332bd311dd2943eafc0b5076c59f816e30
dangerdak <dakshina.scott@gmail.com>
2014-10-03 10:41:58 +0100

    All menus collapsible

--- apuniverse/static/js/menu.js
+++ apuniverse/static/js/menu.js
@@ -16,18 +16,19 @@ $(function() {
 });
 
 $(function() {
-	var menuIcons = $('.side-menu-icon');
-	var menus = $('.links-box nav');
-	var menuHeight = menus.height();
+	$('.links-box').each(function() {
+		var menuIcon = $(this).find('.side-menu-icon');
+		var menu = $(this).find('nav');
 
-	$(menuIcons).bind('click touchstart', function(e) {
-		e.preventDefault();
-		menus.slideToggle();
-	});
-	$(window).resize(function() {
-		var w = $(window).width();
-		if(w > 480 && menus.is(':hidden')) {
-			menus.removeAttr('style');
-		}
+		$(menuIcon).bind('click touchstart', function(e) {
+			e.preventDefault();
+			menu.slideToggle();
+		});
+		$(window).resize(function() {
+			var w = $(window).width();
+			if(w > 480 && menu.is(':hidden')) {
+				menu.removeAttr('style');
+			}
+		});
 	});
 });
